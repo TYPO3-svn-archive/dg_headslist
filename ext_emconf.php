2bc22c1efe04faf1773236000667e66d88ef13ee
dennisg <dennisg@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-27 15:25:51 +0000

    last changes before upload version 1.0.0 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dg_headslist/trunk@11486 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "dg_headslist"
 #
-# Auto generated 22-06-2008 20:15
+# Auto generated 27-08-2008 17:09
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'heads list',
-	'description' => 'a list of heads pictures with a link to the detail page.',
+	'description' => 'A list of heads pictures with Tooltips and a link to a detail page.',
 	'category' => 'plugin',
 	'author' => 'Dennis Grote',
 	'author_email' => 'd.grote@dd-medien.de',
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"8c5a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"83c0";s:17:"ext_localconf.php";s:4:"2ae4";s:14:"ext_tables.php";s:4:"7616";s:14:"ext_tables.sql";s:4:"d5aa";s:16:"flexform_pi1.xml";s:4:"1fbc";s:27:"icon_tx_dgheadslist_cat.gif";s:4:"4cab";s:30:"icon_tx_dgheadslist_cat__h.gif";s:4:"44dd";s:28:"icon_tx_dgheadslist_main.gif";s:4:"dbf9";s:31:"icon_tx_dgheadslist_main__h.gif";s:4:"63d4";s:16:"locallang_db.xml";s:4:"45e4";s:14:"t3mootools.txt";s:4:"082a";s:7:"tca.php";s:4:"6910";s:19:"doc/wizard_form.dat";s:4:"9a3c";s:20:"doc/wizard_form.html";s:4:"e3d5";s:32:"pi1/class.tx_dgheadslist_pi1.php";s:4:"a551";s:17:"pi1/locallang.xml";s:4:"2f0c";s:15:"res/Tips_bg.png";s:4:"2db7";s:12:"res/alert.js";s:4:"d637";s:31:"res/tx_dgheadslist_ToolTips.css";s:4:"8e82";s:22:"res/tmpl/headlist.tmpl";s:4:"3916";}',
+	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"2576";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"83c0";s:17:"ext_localconf.php";s:4:"2ae4";s:14:"ext_tables.php";s:4:"e3f0";s:14:"ext_tables.sql";s:4:"e30f";s:16:"flexform_pi1.xml";s:4:"dbd2";s:27:"icon_tx_dgheadslist_cat.gif";s:4:"9738";s:30:"icon_tx_dgheadslist_cat__h.gif";s:4:"4d30";s:28:"icon_tx_dgheadslist_main.gif";s:4:"dbf9";s:31:"icon_tx_dgheadslist_main__h.gif";s:4:"63d4";s:13:"locallang.xml";s:4:"069f";s:16:"locallang_db.xml";s:4:"1926";s:14:"t3mootools.txt";s:4:"082a";s:7:"tca.php";s:4:"4ccd";s:19:"doc/wizard_form.dat";s:4:"9a3c";s:20:"doc/wizard_form.html";s:4:"e3d5";s:14:"pi1/ce_wiz.gif";s:4:"6d1f";s:32:"pi1/class.tx_dgheadslist_pi1.php";s:4:"bcd6";s:40:"pi1/class.tx_dgheadslist_pi1_wizicon.php";s:4:"1ad2";s:17:"pi1/locallang.xml";s:4:"09e6";s:15:"res/Tips_bg.png";s:4:"2db7";s:15:"res/mootools.js";s:4:"69e5";s:22:"res/tx_dgheadslist.css";s:4:"8155";s:31:"res/tx_dgheadslist_ToolTips.css";s:4:"2c84";s:22:"res/tmpl/headlist.tmpl";s:4:"4f97";}',
 	'suggests' => array(
 	),
 );
