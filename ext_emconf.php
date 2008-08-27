360b196c5c46f6a40c45cc92c01f95d331557d8b
dennisg <dennisg@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-27 15:25:51 +0000

    last changes before upload version 1.0.0 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dg_headslist/tags/version%201.0.0@11486 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "dg_headslist"
+#
+# Auto generated 27-08-2008 17:09
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'heads list',
+	'description' => 'A list of heads pictures with Tooltips and a link to a detail page.',
+	'category' => 'plugin',
+	'author' => 'Dennis Grote',
+	'author_email' => 'd.grote@dd-medien.de',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"2576";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"83c0";s:17:"ext_localconf.php";s:4:"2ae4";s:14:"ext_tables.php";s:4:"e3f0";s:14:"ext_tables.sql";s:4:"e30f";s:16:"flexform_pi1.xml";s:4:"dbd2";s:27:"icon_tx_dgheadslist_cat.gif";s:4:"9738";s:30:"icon_tx_dgheadslist_cat__h.gif";s:4:"4d30";s:28:"icon_tx_dgheadslist_main.gif";s:4:"dbf9";s:31:"icon_tx_dgheadslist_main__h.gif";s:4:"63d4";s:13:"locallang.xml";s:4:"069f";s:16:"locallang_db.xml";s:4:"1926";s:14:"t3mootools.txt";s:4:"082a";s:7:"tca.php";s:4:"4ccd";s:19:"doc/wizard_form.dat";s:4:"9a3c";s:20:"doc/wizard_form.html";s:4:"e3d5";s:14:"pi1/ce_wiz.gif";s:4:"6d1f";s:32:"pi1/class.tx_dgheadslist_pi1.php";s:4:"bcd6";s:40:"pi1/class.tx_dgheadslist_pi1_wizicon.php";s:4:"1ad2";s:17:"pi1/locallang.xml";s:4:"09e6";s:15:"res/Tips_bg.png";s:4:"2db7";s:15:"res/mootools.js";s:4:"69e5";s:22:"res/tx_dgheadslist.css";s:4:"8155";s:31:"res/tx_dgheadslist_ToolTips.css";s:4:"2c84";s:22:"res/tmpl/headlist.tmpl";s:4:"4f97";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ ext_tables.sql
@@ -0,0 +1,48 @@
+#
+# Table structure for table 'tx_dgheadslist_main'
+#
+CREATE TABLE tx_dgheadslist_main (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l18n_parent int(11) DEFAULT '0' NOT NULL,
+	l18n_diffsource mediumblob NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	name tinytext NOT NULL,
+	pic_active blob NOT NULL,
+	pic_inactive blob NOT NULL,
+	categorys blob NOT NULL,
+	link_id tinytext NOT NULL,
+	no_tooltip tinyint(4) DEFAULT '0' NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
+
+
+
+#
+# Table structure for table 'tx_dgheadslist_cat'
+#
+CREATE TABLE tx_dgheadslist_cat (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l18n_parent int(11) DEFAULT '0' NOT NULL,
+	l18n_diffsource mediumblob NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	title tinytext NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
